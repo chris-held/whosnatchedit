95d4b1a70fd7958d01e074478b881baf910c8384
chris held <cheld100@gmail.com>
2019-02-15 14:36:40 -0600

    refactor to use hooks

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,99 +1,82 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class List extends Component {
-  state = {
-    items: []
+const List = ({ items: initialItems }) => {
+  const [items, setItems] = useState(initialItems);
+
+  const addItem = () => {
+    setItems([{ who: "", what: "", id: new Date().getTime() }, ...items]);
   };
 
-  interval;
+  const removeItem = id => {
+    const copy = Array.from(items);
+    setItems(copy.filter(i => i.id !== id));
+  };
 
-  componentDidMount = () => {
-    const { items } = this.props;
-    this.setState({ items });
-    this.interval = setInterval(() => {
-      const { items } = this.state;
+  const handleChange = (index, prop, value) => {
+    const copy = Array.from(items);
+    copy[index][prop] = value;
+    setItems(copy);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
       console.log("items", items);
       window.localStorage.setItem("items", JSON.stringify(items));
     }, 3000);
-  };
-
-  addItem = () => {
-    const { items } = this.state;
-    this.setState({
-      items: [{ who: "", what: "", id: new Date().getTime() }, ...items]
-    });
-  };
-
-  removeItem = id => {
-    const items = Array.from(this.state.items);
-    this.setState({
-      items: items.filter(i => i.id !== id)
-    });
-  };
-
-  handleChange = (index, prop, value) => {
-    const items = Array.from(this.state.items);
-    items[index][prop] = value;
-    this.setState({ items });
-  };
-
-  componentWillUnmount = () => {
-    clearInterval(this.interval);
-  };
-
-  render() {
-    const { items } = this.state;
-    return (
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return (
+    <React.Fragment>
+      <div className="row">
+        <button
+          onClick={addItem}
+          data-testid="List.Add"
+          className="btn btn-primary btn-lg"
+        >
+          Add
+        </button>
+      </div>
       <React.Fragment>
-        <div className="row">
-          <button
-            onClick={this.addItem}
-            data-testid="List.Add"
-            className="btn btn-primary btn-lg"
-          >
-            Add
-          </button>
-        </div>
-        <React.Fragment>
-          {items.map((item, index) => (
-            <div className="row" key={item.id} data-testid="ListItem">
-              <form>
-                <input
-                  type="text"
-                  data-testid="ListItem.WhoInput"
-                  className="input-small"
-                  onChange={e => {
-                    this.handleChange(index, "who", e.target.value);
-                  }}
-                  value={item.who}
-                  placeholder="Who"
-                />
-                <input
-                  type="text"
-                  className="input-small"
-                  onChange={e => {
-                    this.handleChange(index, "what", e.target.value);
-                  }}
-                  value={item.what}
-                  placeholder="What"
-                />
-                <button
-                  type="button"
-                  data-testid="ListItem.Remove"
-                  onClick={() => {
-                    this.removeItem(item.id);
-                  }}
-                  className="btn btn-outline-danger"
-                >
-                  X
-                </button>
-              </form>
-            </div>
-          ))}
-        </React.Fragment>
+        {items.map((item, index) => (
+          <div className="row" key={item.id} data-testid="ListItem">
+            <form>
+              <input
+                type="text"
+                data-testid="ListItem.WhoInput"
+                className="input-small"
+                onChange={e => {
+                  handleChange(index, "who", e.target.value);
+                }}
+                value={item.who}
+                placeholder="Who"
+              />
+              <input
+                type="text"
+                className="input-small"
+                onChange={e => {
+                  handleChange(index, "what", e.target.value);
+                }}
+                value={item.what}
+                placeholder="What"
+              />
+              <button
+                type="button"
+                data-testid="ListItem.Remove"
+                onClick={() => {
+                  removeItem(item.id);
+                }}
+                className="btn btn-outline-danger"
+              >
+                X
+              </button>
+            </form>
+          </div>
+        ))}
       </React.Fragment>
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
 
 export default List;
